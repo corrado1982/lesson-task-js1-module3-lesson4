cab702d234efc03d1f478c01d8692b7a847718ff
corrado1982 <104769882+corrado1982@users.noreply.github.com>
2022-11-03 15:00:42 +0100

    finished

--- script2.js
+++ script2.js
@@ -1,8 +1,16 @@
 
 const API_URL = "https://mashape-community-urban-dictionary.p.rapidapi.com/define?term=wat";
 
+const options = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': '1dd78a3fdbmsha2d293fec8f8c53p16f7a3jsn37a1e5d9aea9',
+		
+	}
+};
+
 async function callUrbanDictionary() {
-  const response = await fetch(API_URL);
+  const response = await fetch(API_URL, options);
   const results = await response.json();
   console.log(results);
 }
